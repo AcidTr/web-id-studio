71987ac1f8bb8bc7402d13b9be19b9f681af3593
Victor Mourão <35748259+AcidTr@users.noreply.github.com>
2020-09-12 15:17:46 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App: React.FC = () => (
+  <>
+    <h1>ReactJS Typescript Template</h1>
+    <p>Editorconfig, ESlint and Prettier Already configured!</p>
+  </>
+);
+
+export default App;
