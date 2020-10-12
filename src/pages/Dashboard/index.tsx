9f34f3b3188cbeb7b87f891d0b802c8408b6cef7
Victor Mourão <pvictormourao@gmail.com>
2020-10-11 21:20:57 -0300

    testing create appointment

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -5,7 +5,7 @@ import React, {
   useState,
   useRef,
 } from 'react';
-import { isToday, format, parseISO, isAfter } from 'date-fns';
+import { isToday, format, parseISO, isAfter, formatISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import DayPicker, { DayModifiers } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
@@ -133,20 +133,17 @@ const Dashboard: React.FC = () => {
         const month = selectedDate.getMonth();
         const day = selectedDate.getDate();
 
-        const date = new Date(
-          year,
-          month,
-          day,
-          Number(hour),
-          Number(minute),
-        ).toISOString();
+        const date = formatISO(
+          new Date(year, month, day, Number(hour), Number(minute)),
+        );
+        console.log(date);
 
-        await api.post('/appointments', {
-          name: data.name,
-          phone: data.phone,
-          provider_id: providerId,
-          date,
-        });
+        // await api.post('/appointments', {
+        //   name: data.name,
+        //   phone: data.phone,
+        //   provider_id: providerId,
+        //   date,
+        // });
 
         addToast({
           type: 'success',
